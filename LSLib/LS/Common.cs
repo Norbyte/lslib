bdce21410d5f28ce5a45c89077188dc33ed0572a
Norbyte <infernorb@gmail.com>
2017-09-16 15:40:20 +0200

    Release v1.8.3

--- LSLib/LS/Common.cs
+++ LSLib/LS/Common.cs
@@ -10,7 +10,7 @@ namespace LSLib.LS
     {
         public const int MajorVersion = 1;
         public const int MinorVersion = 8;
-        public const int PatchVersion = 2;
+        public const int PatchVersion = 3;
 
         /// <summary>
         /// Returns the version number of the LSLib library
