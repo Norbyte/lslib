0ad545a5a0a936e76f32f869533400a6a9b0d9b4
Norbyte <infernorb@gmail.com>
2018-08-04 22:49:32 +0200

    Add connection/command timeout

--- RconClient/Rcon.cs
+++ RconClient/Rcon.cs
@@ -1,12 +1,15 @@
 ﻿using LSLib.Rcon.DosPackets;
 using System;
 using System.Net;
+using System.Timers;
 
 namespace LSLib.Rcon
 {
     public class RconApp
     {
         static private bool Executed = false;
+        static private bool ReceivedEvents = false;
+        static private RakNetSession Session;
         static private string Command;
         static private string[] Arguments;
 
@@ -29,9 +32,38 @@ namespace LSLib.Rcon
             var socket = new RakNetSocket();
             socket.SessionEstablished += OnSessionEstablished;
 
-            // sending data (for the sake of simplicity, back to ourselves):
+            // Create a disconnect timer to make sure that we disconnect after the last console message
+            var timer = new System.Timers.Timer(3000);
+            timer.Elapsed += OnTimedEvent;
+            timer.Enabled = true;
+            
             IPEndPoint target = new IPEndPoint(IPAddress.Parse(ipPort[0]), port);
             socket.BeginConnection(target);
+        }
+
+        static void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            if (!ReceivedEvents)
+            {
+                ReceivedEvents = true;
+
+                if (Session != null)
+                {
+                    Console.WriteLine("Disconnecting.");
+                    var disconnectCmd = new DosDisconnectConsole();
+                    Session.SendEncapsulated(disconnectCmd, EncapsulatedReliability.ReliableOrdered);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Timed out waiting for session establishment");
+                    Console.ResetColor();
+                }
+            }
+            else
+            {
+                ReceivedEvents = false;
+            }
         }
 
         static Packet OnPacketParse(Byte id)
@@ -51,6 +83,7 @@ namespace LSLib.Rcon
             session.PacketConstructor += OnPacketParse;
             session.PacketReceived += OnPacketReceived;
             session.SessionDisconnected += OnSessionDisconnected;
+            Session = session;
         }
 
         static void OnSessionDisconnected(RakNetSession session)
@@ -77,7 +110,7 @@ namespace LSLib.Rcon
             }
             else if (packet is DosUnknown8B)
             {
-                Console.WriteLine("Rcon payload received.");
+                Console.WriteLine("Sending console command:");
                 Console.WriteLine("> " + Command + " " + String.Join(" ", Arguments));
                 var consoleCmd = new DosSendConsoleCommand
                 {
@@ -85,34 +118,35 @@ namespace LSLib.Rcon
                     Arguments = Arguments
                 };
                 session.SendEncapsulated(consoleCmd, EncapsulatedReliability.ReliableOrdered);
+                ReceivedEvents = true;
             }
             else if (packet is DosConsoleResponse)
             {
-                Console.WriteLine("Console response:");
+                bool hasResult = false;
                 var lines = (packet as DosConsoleResponse).Lines;
                 foreach (var line in lines)
                 {
                     switch (line.Level)
                     {
-                        case 4: Console.ForegroundColor = ConsoleColor.Green; break;
-                        case 5: Console.ForegroundColor = ConsoleColor.Red; break;
+                        case 4: Console.ForegroundColor = ConsoleColor.Green; hasResult = true; break;
+                        case 5: Console.ForegroundColor = ConsoleColor.Red; hasResult = true; break;
                         default: Console.ResetColor(); break;
                     }
                     Console.WriteLine(line.Line);
                 }
                 Console.ResetColor();
                 Executed = true;
+                ReceivedEvents = true;
 
-                Console.WriteLine("Disconnecting Rcon.");
-                var disconnectCmd = new DosDisconnectConsole();
-                session.SendEncapsulated(disconnectCmd, EncapsulatedReliability.ReliableOrdered);
+                if (hasResult)
+                {
+                    var disconnectCmd = new DosDisconnectConsole();
+                    Session.SendEncapsulated(disconnectCmd, EncapsulatedReliability.ReliableOrdered);
+                }
             }
             else
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(" --> (UNHANDLED ENCAP PACKET) ");
-                Console.WriteLine(packet);
-                Console.ResetColor();
+                throw new Exception("Unhandled DOS encapsulated packet");
             }
         }
     }
