8b35a11942fee83915f312d677deace918eab2d5
Norbyte <infernorb@gmail.com>
2018-10-01 17:20:57 +0200

    Add story decompiler tool

---
+++ StoryDecompiler/Arguments.cs
@@ -0,0 +1,28 @@
+﻿using CommandLineParser.Arguments;
+using System;
+
+namespace LSTools.StoryDecompiler
+{
+    public class CommandLineArguments
+    {
+        [ValueArgument(typeof(string), "input",
+            Description = "Compiled story/savegame file path",
+            ValueOptional = false,
+            Optional = false
+        )]
+        public string InputPath;
+
+        [ValueArgument(typeof(string), "output",
+            Description = "Goal output directory",
+            ValueOptional = false,
+            Optional = false
+        )]
+        public string OutputPath;
+
+        [SwitchArgument("debug-log", false,
+            Description = "Generate story debug log",
+            Optional = true
+        )]
+        public bool DebugLog;
+    }
+}
