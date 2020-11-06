c13d00276f5c15177f9c5c0f2c54e6f4da7745ac
Norbyte <infernorb@gmail.com>
2020-11-06 22:17:44 +0100

    Display location information for LSX parsing errors

--- LSLib/LS/LSXReader.cs
+++ LSLib/LS/LSXReader.cs
@@ -50,6 +50,10 @@ namespace LSLib.LS
         private Stream stream;
         private XmlReader reader;
         private uint Version;
+        private Resource resource;
+        private Region currentRegion;
+        private List<Node> stack;
+        private int lastLine, lastColumn;
 
         public LSXReader(Stream stream)
         {
@@ -114,166 +118,211 @@ namespace LSLib.LS
             }
         }
 
-        public Resource Read()
+        private void ReadElement()
+        {
+            switch (reader.Name)
+            {
+                case "save":
+                    // Root element
+                    if (stack.Count() > 0)
+                        throw new InvalidFormatException("Node <save> was unexpected.");
+                    break;
+
+                case "header":
+                    // LSX metadata part 1
+                    resource.Metadata.Timestamp = Convert.ToUInt64(reader["time"]);
+                    break;
+
+                case "version":
+                    // LSX metadata part 2
+                    resource.Metadata.MajorVersion = Convert.ToUInt32(reader["major"]);
+                    resource.Metadata.MinorVersion = Convert.ToUInt32(reader["minor"]);
+                    resource.Metadata.Revision = Convert.ToUInt32(reader["revision"]);
+                    resource.Metadata.BuildNumber = Convert.ToUInt32(reader["build"]);
+                    Version = resource.Metadata.MajorVersion;
+                    break;
+
+                case "region":
+                    if (currentRegion != null)
+                        throw new InvalidFormatException("A <region> can only start at the root level of a resource.");
+
+                    Debug.Assert(!reader.IsEmptyElement);
+                    var region = new Region();
+                    region.RegionName = reader["id"];
+                    Debug.Assert(region.RegionName != null);
+                    resource.Regions.Add(region.RegionName, region);
+                    currentRegion = region;
+                    break;
+
+                case "node":
+                    if (currentRegion == null)
+                        throw new InvalidFormatException("A <node> must be located inside a region.");
+
+                    Node node;
+                    if (stack.Count() == 0)
+                    {
+                        // The node is the root node of the region
+                        node = currentRegion;
+                    }
+                    else
+                    {
+                        // New node under the current parent
+                        node = new Node();
+                        node.Parent = stack.Last();
+                    }
+
+                    node.Name = reader["id"];
+                    Debug.Assert(node.Name != null);
+                    if (node.Parent != null)
+                        node.Parent.AppendChild(node);
+
+                    if (!reader.IsEmptyElement)
+                        stack.Add(node);
+                    break;
+
+                case "attribute":
+                    UInt32 attrTypeId;
+                    if (Version >= 4)
+                    {
+                        attrTypeId = (uint)TypeNames[reader["type"]];
+                    }
+                    else
+                    {
+                        attrTypeId = Convert.ToUInt32(reader["type"]);
+                    }
+
+                    var attrName = reader["id"];
+                    if (attrTypeId > (int)NodeAttribute.DataType.DT_Max)
+                        throw new InvalidFormatException(String.Format("Unsupported attribute data type: {0}", attrTypeId));
+
+                    Debug.Assert(attrName != null);
+                    var attr = new NodeAttribute((NodeAttribute.DataType)attrTypeId);
+
+                    var attrValue = reader["value"];
+                    if (attrValue != null)
+                    {
+                        attr.FromString(attrValue);
+                    }
+
+                    if (attr.Type == NodeAttribute.DataType.DT_TranslatedString)
+                    {
+                        if (attr.Value == null)
+                        {
+                            attr.Value = new TranslatedString();
+                        }
+
+                        var ts = ((TranslatedString)attr.Value);
+                        ts.Handle = reader["handle"];
+                        Debug.Assert(ts.Handle != null);
+
+                        if (attrValue == null)
+                        {
+                            ts.Version = UInt16.Parse(reader["version"]);
+                        }
+                    }
+                    else if (attr.Type == NodeAttribute.DataType.DT_TranslatedFSString)
+                    {
+                        var fs = ((TranslatedFSString)attr.Value);
+                        ReadTranslatedFSString(fs);
+                    }
+
+                    stack.Last().Attributes.Add(attrName, attr);
+                    break;
+
+                case "children":
+                    // Child nodes are handled in the "node" case
+                    break;
+
+                default:
+                    throw new InvalidFormatException(String.Format("Unknown element encountered: {0}", reader.Name));
+            }
+        }
+
+        private void ReadEndElement()
+        {
+            switch (reader.Name)
+            {
+                case "save":
+                case "header":
+                case "version":
+                case "attribute":
+                case "children":
+                    // These elements don't change the stack, just discard them
+                    break;
+
+                case "region":
+                    Debug.Assert(stack.Count == 0);
+                    Debug.Assert(currentRegion != null);
+                    Debug.Assert(currentRegion.Name != null);
+                    currentRegion = null;
+                    break;
+
+                case "node":
+                    stack.RemoveAt(stack.Count - 1);
+                    break;
+
+                default:
+                    throw new InvalidFormatException(String.Format("Unknown element encountered: {0}", reader.Name));
+            }
+        }
+
+        private void ReadInternal()
         {
             using (this.reader = XmlReader.Create(stream))
             {
-                Resource rsrc = new Resource();
-                Region currentRegion = null;
-                List<Node> stack = new List<Node>();
-
-                while (reader.Read())
+                try
                 {
-                    if (reader.NodeType == XmlNodeType.Element)
+                    while (reader.Read())
                     {
-                        switch (reader.Name)
+                        if (reader.NodeType == XmlNodeType.Element)
                         {
-                            case "save":
-                                // Root element
-                                if (stack.Count() > 0)
-                                    throw new InvalidFormatException("Node <save> was unexpected.");
-                                break;
-
-                            case "header":
-                                // LSX metadata part 1
-                                rsrc.Metadata.Timestamp = Convert.ToUInt64(reader["time"]);
-                                break;
-
-                            case "version":
-                                // LSX metadata part 2
-                                rsrc.Metadata.MajorVersion = Convert.ToUInt32(reader["major"]);
-                                rsrc.Metadata.MinorVersion = Convert.ToUInt32(reader["minor"]);
-                                rsrc.Metadata.Revision = Convert.ToUInt32(reader["revision"]);
-                                rsrc.Metadata.BuildNumber = Convert.ToUInt32(reader["build"]);
-                                Version = rsrc.Metadata.MajorVersion;
-                                break;
-
-                            case "region":
-                                if (currentRegion != null)
-                                    throw new InvalidFormatException("A <region> can only start at the root level of a resource.");
-
-                                Debug.Assert(!reader.IsEmptyElement);
-                                var region = new Region();
-                                region.RegionName = reader["id"];
-                                Debug.Assert(region.RegionName != null);
-                                rsrc.Regions.Add(region.RegionName, region);
-                                currentRegion = region;
-                                break;
-
-                            case "node":
-                                if (currentRegion == null)
-                                    throw new InvalidFormatException("A <node> must be located inside a region.");
-
-                                Node node;
-                                if (stack.Count() == 0)
-                                {
-                                    // The node is the root node of the region
-                                    node = currentRegion;
-                                }
-                                else
-                                {
-                                    // New node under the current parent
-                                    node = new Node();
-                                    node.Parent = stack.Last();
-                                }
-
-                                node.Name = reader["id"];
-                                Debug.Assert(node.Name != null);
-                                if (node.Parent != null)
-                                    node.Parent.AppendChild(node);
-
-                                if (!reader.IsEmptyElement)
-                                    stack.Add(node);
-                                break;
-
-                            case "attribute":
-                                UInt32 attrTypeId;
-                                if (Version >= 4)
-                                {
-                                    attrTypeId = (uint)TypeNames[reader["type"]];
-                                }
-                                else
-                                {
-                                    attrTypeId = Convert.ToUInt32(reader["type"]);
-                                }
-
-                                var attrName = reader["id"];
-                                if (attrTypeId > (int)NodeAttribute.DataType.DT_Max)
-                                    throw new InvalidFormatException(String.Format("Unsupported attribute data type: {0}", attrTypeId));
-
-                                Debug.Assert(attrName != null);
-                                var attr = new NodeAttribute((NodeAttribute.DataType)attrTypeId);
-
-                                var attrValue = reader["value"];
-                                if (attrValue != null)
-                                {
-                                    attr.FromString(attrValue);
-                                }
-
-                                if (attr.Type == NodeAttribute.DataType.DT_TranslatedString)
-                                {
-                                    if (attr.Value == null)
-                                    {
-                                        attr.Value = new TranslatedString();
-                                    }
-
-                                    var ts = ((TranslatedString)attr.Value);
-                                    ts.Handle = reader["handle"];
-                                    Debug.Assert(ts.Handle != null);
-
-                                    if (attrValue == null)
-                                    {
-                                        ts.Version = UInt16.Parse(reader["version"]);
-                                    }
-                                }
-                                else if (attr.Type == NodeAttribute.DataType.DT_TranslatedFSString)
-                                {
-                                    var fs = ((TranslatedFSString)attr.Value);
-                                    ReadTranslatedFSString(fs);
-                                }
-
-                                stack.Last().Attributes.Add(attrName, attr);
-                                break;
-
-                            case "children":
-                                // Child nodes are handled in the "node" case
-                                break;
-
-                            default:
-                                throw new InvalidFormatException(String.Format("Unknown element encountered: {0}", reader.Name));
+                            ReadElement();
+                        }
+                        else if (reader.NodeType == XmlNodeType.EndElement)
+                        {
+                            ReadEndElement();
                         }
                     }
-                    else if (reader.NodeType == XmlNodeType.EndElement)
-                    {
-                        switch (reader.Name)
-                        {
-                            case "save":
-                            case "header":
-                            case "version":
-                            case "attribute":
-                            case "children":
-                                // These elements don't change the stack, just discard them
-                                break;
-
-                            case "region":
-                                Debug.Assert(stack.Count == 0);
-                                Debug.Assert(currentRegion != null);
-                                Debug.Assert(currentRegion.Name != null);
-                                currentRegion = null;
-                                break;
-
-                            case "node":
-                                stack.RemoveAt(stack.Count - 1);
-                                break;
-
-                            default:
-                                throw new InvalidFormatException(String.Format("Unknown element encountered: {0}", reader.Name));
-                        }
-                    }
+                } catch (Exception e)
+                {
+                    lastLine = ((IXmlLineInfo)reader).LineNumber;
+                    lastColumn = ((IXmlLineInfo)reader).LinePosition;
+                    throw;
                 }
-
-                return rsrc;
             }
+        }
+
+        public Resource Read()
+        {
+            resource = new Resource();
+            currentRegion = null;
+            stack = new List<Node>();
+            lastLine = lastColumn = 0;
+            var resultResource = resource;
+
+            try
+            {
+                ReadInternal();
+            }
+            catch (Exception e)
+            {
+                if (lastLine > 0)
+                {
+                    throw new Exception($"Parsing error at or near line {lastLine}, column {lastColumn}:{Environment.NewLine}{e.Message}", e);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            finally
+            {
+                resource = null;
+                currentRegion = null;
+                stack = null;
+            }
+
+            return resultResource;
         }
     }
 }
