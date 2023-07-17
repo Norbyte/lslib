1df7280de4f4c12b017d590373da028c9f89cd6c
Pierre Marsais <pim@libdl.so>
2023-07-17 16:40:49 +0200

    Make LZ4FrameCompressor and Granny2Compressor throw NotSupportedException when used

--- LSLibNativeLinux/Class1.cs
+++ LSLibNativeLinux/Class1.cs
@@ -1,24 +1,29 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace LSLib.Native
 {
 public class LZ4FrameCompressor {
 	public static byte[] Compress(byte[] compressed) {
+		throw new NotSupportedException("LZ4 compression is currently unsupported");
 		return compressed;
 	}
 	public static byte[] Decompress(byte[] compressed) {
+		throw new NotSupportedException("LZ4 decompression is currently unsupported");
 		return compressed;
 	}
 }
 
 public class Granny2Compressor {
 	public static byte[] Decompress(Int32 format, byte[] compressed, Int32 decompressedSize, Int32 stop0, Int32 stop1, Int32 stop2) {
+		throw new NotSupportedException("GR2 decompression is currently unsupported");
 		return compressed;
 	}
 
 	public static byte[] Decompress4(byte[] compressed, Int32 decompressedSize) {
+		throw new NotSupportedException("GR2 decompression is currently unsupported");
 		return compressed;
 	}
-
 }
 }
