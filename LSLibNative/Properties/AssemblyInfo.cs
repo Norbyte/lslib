8cdcd292453cb2dd7313d818f627846c3efdb210
Pierre Marsais <pim@libdl.so>
2023-07-17 23:49:14 +0200

    Rename LSLibNativeLinux in LSLibNative

--- LSLibNative/Properties/AssemblyInfo.cs
+++ LSLibNative/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LSLibNativeLinux")]
+[assembly: AssemblyTitle("LSLibNative")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
