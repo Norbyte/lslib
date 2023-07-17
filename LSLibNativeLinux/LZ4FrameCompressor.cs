783441a951f9ba76f71eddc19bc7aee78d95989a
Pierre Marsais <pim@libdl.so>
2023-07-17 23:46:56 +0200

    Have each class in Native namespace in their own files

--- LSLibNativeLinux/LZ4FrameCompressor.cs
+++ LSLibNativeLinux/LZ4FrameCompressor.cs
@@ -102,16 +102,4 @@ public class LZ4FrameCompressor {
 		return output;
 	}
 }
-
-public class Granny2Compressor {
-	public static byte[] Decompress(Int32 format, byte[] compressed, Int32 decompressedSize, Int32 stop0, Int32 stop1, Int32 stop2) {
-		throw new NotSupportedException("GR2 decompression is currently unsupported");
-		return compressed;
-	}
-
-	public static byte[] Decompress4(byte[] compressed, Int32 decompressedSize) {
-		throw new NotSupportedException("GR2 decompression is currently unsupported");
-		return compressed;
-	}
-}
 }
